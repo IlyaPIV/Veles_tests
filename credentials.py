40ae57baa0ad1e9cf5a6d5a8096122c41bfc314f
Ilya PIV <petermoloch@mail.ru>
2025-08-06 20:40:39 +0200

    alpha

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 GOOGLE_ACC = 'your_google_email@gmail.com'
-GOOGLE_PASS = 'your_google_acc_password'
+GOOGLE_PASS = 'email_pass'
 VELES_LOGIN = 'your_email'
 VELES_PASSWORD = 'Veles_Pass'
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 import pandas as pd
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 
 import credentials
@@ -11,9 +12,7 @@ from seleniumbase import SB
 
 
 VELES_URL = 'https://veles.finance/share/tRlrW'
-TEST_VERSION = 'Python Test v.0.0.0'
-
-
+AUTH_BY_GOOGLE = True
 
 def match_label_to_column(label_text):
     match label_text:
@@ -104,70 +103,110 @@ def open_veles_page(driver):
     wait.until(EC.presence_of_element_located((By.ID, 'googleAuth')))
 
     try:
-        # часть со входом обычным
-        # email_input = driver.find_element(By.NAME, 'username')
-        # email_input.send_keys(VELES_LOGIN)
-        # pass_input = driver.find_element(By.NAME, 'password')
-        # pass_input.send_keys(VELES_PASSWORD)
-        # enter_btn = driver.find_element(By.ID, 'submitButton')
-        # enter_btn.click()
+        if AUTH_BY_GOOGLE:
+            # часть со входом через гугл auth
+            driver.find_element(By.ID, 'googleAuth').click()
+            driver.type("#identifierId", credentials.GOOGLE_ACC)
+            driver.click("#identifierNext > div > button")
+            driver.type("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input", credentials.GOOGLE_PASS)
+            driver.click("#passwordNext > div > button")
+            time.sleep(5)
+            driver.find_element(By.XPATH, '//*[@id="app"]/main/section[4]/div[2]/ul/li[3]').click()
+            time.sleep(2)
+            driver.find_element(By.CSS_SELECTOR, '#app > main > section.backtest-tabs > div.backtest-tabs-deals.backtest-tabs > div > div > div.table-wrapper > div > div.head > div:nth-child(18)').click()
+            print('We are ready to start!')
+        else:
+            # часть со входом обычным
+            email_input = driver.find_element(By.NAME, 'username')
+            email_input.send_keys(credentials.VELES_LOGIN)
+            pass_input = driver.find_element(By.NAME, 'password')
+            pass_input.send_keys(credentials.VELES_PASSWORD)
+            enter_btn = driver.find_element(By.ID, 'submitButton')
+            enter_btn.click()
 
-        # часть со входом через гугл auth
-        driver.find_element(By.ID, 'googleAuth').click()
-        driver.type("#identifierId", credentials.GOOGLE_ACC)
-        driver.click("#identifierNext > div > button")
-        driver.type("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input", credentials.GOOGLE_PASS)
-        driver.click("#passwordNext > div > button")
-        time.sleep(5)
-        driver.find_element(By.XPATH, '//*[@id="app"]/main/section[4]/div[2]/ul/li[3]').click()
-        time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR, '#app > main > section.backtest-tabs > div.backtest-tabs-deals.backtest-tabs > div > div > div.table-wrapper > div > div.head > div:nth-child(18)').click()
-        print('We are ready to start!')
         return True
+
     except Exception as e:
         print(f"The Shit happens: {e}")
         return False
 
 
+def find_element(driver, by: str, value: str) -> WebElement:
+    element: WebElement = None
+    while not element:
+        element= driver.find_element(by, value)
+    return element
+
+def find_elements(driver, by: str, value: str) -> WebElement:
+    elements: WebElement = None
+    while not elements:
+        elements= driver.find_elements(by, value)
+    return elements
+
+
 def proceed_next_coin(coin, driver, strategy_url, strategy_name):
-    # загружаем образец стратегии и ждём 5 секунд
+    # загружаем образец бота
     driver.get(strategy_url)
-    time.sleep(7)
+    WebDriverWait(driver, 20).until(
+        lambda d: d.execute_script("return document.readyState") == "complete"
+    )
+    time.sleep(2)  # 2 sec
     try:
-        # кнопка "Запустить" +7 sec
-        driver.find_element(By.CLASS_NAME, 'backtest__run-bot').click()
-        time.sleep(5)
+        (find_element(driver, By.CSS_SELECTOR, '#app > main > section.backtest-header > div.backtest-header-middle.items-center > div.backtest-header-middle-buttons > button.backtest__run-bot.cursor-pointer.orange.button')
+                    .click())
+        time.sleep(3)  # 5 sec
+
         # меняем название теста добавляя монету и стратегию +12 sec
-        driver.find_element(By.XPATH,
-                            '//*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div[1]/label/input').send_keys(
-                                                ' # ' + coin + ' (' + strategy_name + ')')
-        time.sleep(2)
-        # Открываем выпадающий список монет +14 sec
-        driver.find_element(By.XPATH,
-                            '//*[@id="app"]/div/div[2]/div[2]/div[2]/div[1]/div[2]/div[2]/div[3]/div/div[1]/label/div').click()
-        time.sleep(2)
-        # Находим нужную монету в списке и кликаем +16 sec
+        (find_element(driver, By.CSS_SELECTOR, '#app > div > div.bot-editor__body > div.bot-editor__form > div.bot-editor__form-body > div.bot-form-block.bot-form-basic > div.bot-form-block__body > div.bot-form-basic__section.bot-form-basic__col > label > input[type=text]')
+                    .send_keys(' # ' + coin + ' (' + strategy_name + ')'))
+        # driver.find_element(By.CSS_SELECTOR,
+        #                     '#app > div > div.bot-editor__body > div.bot-editor__form > div.bot-editor__form-body > div.bot-form-block.bot-form-basic > div.bot-form-block__body > div.bot-form-basic__section.bot-form-basic__col > label > input[type=text]').send_keys(
+        #                                         ' # ' + coin + ' (' + strategy_name + ')')
+        time.sleep(3) # 8 sec
+
+        # Открываем выпадающий список монет
+        (find_element(driver, By.CSS_SELECTOR, 'div.wrapper.pair.bot-form-basic__section > div.pair__bottom-wrapper > div > div:nth-child(1) > label > i')
+                    .click())
+        # driver.find_element(By.CSS_SELECTOR,
+        #                     'div.wrapper.pair.bot-form-basic__section > div.pair__bottom-wrapper > div > div:nth-child(1) > label > i').click()
+        time.sleep(3) #11
+
+        # Находим нужную монету в списке и кликаем
         xpath = "//div[@class='select-item'][.//span[text()='" + coin + "']]"
-        driver.find_element(By.XPATH, xpath).click()
-        time.sleep(2)
-        # Жмём запустить бэктест +18 sec
-        driver.find_element(By.CLASS_NAME, 'bot-form-footer__backtests-button-name').click()
-        time.sleep(2)
-        # Убираем галочку "Публичный тест" +20 sec
-        driver.find_element(By.XPATH,
-                            '//*[@id="app"]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div[2]/div[2]/div[1]/label[1]/div').click()
-        time.sleep(2)
+        find_element(driver, By.XPATH, xpath).click()
+        # driver.find_element(By.XPATH, xpath).click()
+        time.sleep(3) #14
+
+        # Жмём запустить бэктест
+        find_element(driver, By.CLASS_NAME, 'bot-form-footer__backtests-button-name').click()
+        # driver.find_element(By.CLASS_NAME, 'bot-form-footer__backtests-button-name').click()
+        time.sleep(3) #17
+
+        # Убираем галочку "Публичный тест"
+        (find_element(driver, By.XPATH, "//div[@class='selects-wrapper']/label[contains(., 'Публичный тест')]/div[@class='checkbox']")
+                    .click() )
+        time.sleep(2) #19
+
         # Запускаем тест нажав на "Подтвердить" +22 sec
-        driver.find_element(By.XPATH, "//div[@class='popup-footer']//button").click()
-        time.sleep(5)
+        (find_element(driver, By.XPATH, "//div[@class='popup-footer']//button")
+                .click())
+        # driver.find_element(By.XPATH, "//div[@class='popup-footer']//button").click()
+        time.sleep(5) #24
+
         # Жмём кнопку "Поделиться" 27 sec
-        driver.find_element(By.CLASS_NAME, 'backtest-share').click()
-        time.sleep(2)
+        (find_element(driver, By.CLASS_NAME, 'backtest-share')
+                .click())
+        # driver.find_element(By.CLASS_NAME, 'backtest-share').click()
+        time.sleep(3) #27
+
         # Выделяем URL теста и сохраняем его +29 sec
-        driver.find_element(By.XPATH, '//*[@id="app"]/div[4]/div[2]/div[2]/label/div').click()
+        (find_element(driver, By.CSS_SELECTOR, '#app > div.popup-wrapper.add-telegram-popup.active > div.popup > div.popup-body > label > div')
+                .click())
+        time.sleep(2) #29
         link = driver.execute_script("return window.getSelection().toString();")
-        time.sleep(1)
-        # Просто ждём ещё 1 секунду +30 sec
+        print(link)
+        time.sleep(2) #31
+
         return link
 
     except Exception as e:
@@ -181,51 +220,82 @@ def run_the_test():
     print(f'Total Coins to test: {len(coins_list)}')
 
     if len(coins_list) > 0:
-        tested_coins = []
-        test_links = []
 
         tests_list = pd.read_csv('to_test.csv')
 
-        with SB(uc=True) as opera_driver:
+        chrome_args = [
+            "--disable-sync",
+            "--disable-notifications",
+            "--no-default-browser-check",
+            "--no-first-run",
+            "--disable-infobars",
+            "--start-maximized",
+            "--disable-blink-features=AutomationControlled"
+        ]
+        with SB(uc=True, browser="chrome", enable_sync=False, chromium_arg=chrome_args) as opera_driver:
             if open_veles_page(opera_driver):
+                tests_dirs = []
                 # we are logged and ready
                 for (i, test) in tests_list.iterrows():
+                    tested_coins = []
+                    test_links = []
+
                     test_url = test['url']
                     strategy_name = test['strategy']
                     for coin_name in coins_list:
                         # тест для монеты по стратегии
                         tested_coins.append(coin_name)
-                        backtest_link = proceed_next_coin(coin_name, opera_driver, test_url, strategy_name)
+                        try:
+                            backtest_link = proceed_next_coin(coin_name, opera_driver, test_url, strategy_name)
+                        except Exception as e:
+                            backtest_link = 'Error'
                         test_links.append(backtest_link)
-        print('Test is finished. Saving results...')
-        results = {
-            "Coin": tested_coins,
-            "Link": test_links
-        }
 
-        df = pd.DataFrame(results)
+                    print('Test is finished. Saving results...')
+                    results = {
+                        "Coin": tested_coins,
+                        "Link": test_links
+                    }
 
-        test_dir = './'+strategy_name
-        if not os.path.isdir(test_dir):
-            os.makedirs(test_dir)
-        backtests_result_file_name = './' + strategy_name + '/tests.csv'
-        df.to_csv(backtests_result_file_name, index=False)
-        print('Links are saved...')
-        time.sleep(1)
-        backtests_parsed_file_name = './' + strategy_name + '/results (parsed).csv'
-        parse_results(opera_driver, df, backtests_parsed_file_name)
-        print('Parsing is finished...')
+                    df = pd.DataFrame(results)
+
+                    test_dir = './'+strategy_name
+                    if not os.path.isdir(test_dir):
+                        os.makedirs(test_dir)
+                    backtests_result_file_name = './' + strategy_name + '/tests.csv'
+                    df.to_csv(backtests_result_file_name, index=False)
+                    tests_dirs.append(test_dir)
+                    print('Links are saved...')
+                    time.sleep(1)
+
+                for test_dir in tests_dirs:
+                    results_file = test_dir + '/tests.csv'
+                    df = pd.read_csv(results_file)
+                    backtests_parsed_file_name = test_dir + '/results (parsed).csv'
+                    parse_results(opera_driver, df, backtests_parsed_file_name)
+            print('Parsing is finished...')
 
 
 def only_parsing():
-    with SB(uc=True) as opera_driver:
+
+    chrome_args = [
+        "--disable-sync",
+        "--disable-notifications",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--disable-infobars"
+        # "--incognito",
+        # f"--user-data-dir={profile_path}",
+        # f"--profile-directory={profile_name}"
+    ]
+    with SB(uc=True, browser_args=chrome_args) as opera_driver:
         if open_veles_page(opera_driver):
+            opera_driver.maximize_window()
             df = pd.read_csv('backtests.csv')
-            backtests_parsed_file_name = TEST_VERSION + ' (parsed).csv'
+            backtests_parsed_file_name = 'results (parsed).csv'
             parse_results(opera_driver, df, backtests_parsed_file_name)
             print('Parsing is finished...')
 
 
 if __name__ == '__main__':
-    #run_the_test()
-    only_parsing()
+    run_the_test()
